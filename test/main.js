bed9f9fdd3832df5961ecb4b700f30cb04266663
Angela Panfil <panfia1@gmail.com>
2013-04-02 16:52:43 -0400

    added first function and set of tests

--- test/main.js
+++ test/main.js
@@ -1,2 +1,34 @@
-var artsy = require('../lib/main'),
+var lookbook = require('../lib/main'),
     assert = require('assert');
+
+describe('getLooks', function() {
+
+    it('getLooks with callback', function(done) {
+
+        lookbook.getLooks(undefined, function(looks) {
+            assert.ok(looks && looks['looks'].length > 0);
+            done();
+        });
+
+    });
+
+    it('getLooks with promise', function(done) {
+
+        lookbook.getLooks().then(function(looks) {
+            assert.ok(looks && looks['looks'].length > 0);
+            done();
+        });
+
+    });
+    
+     it('getLooks with pagination', function(done) {
+
+        lookbook.getLooks(2, function(looks) {
+            assert.ok(looks && looks['looks'].length > 0);
+            done();
+        });
+
+    });
+
+
+});
